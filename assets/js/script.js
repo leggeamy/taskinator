ae6e30b3701b750a1b6dfd60c22863a57532602f
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-09-25 17:15:27 -0400

    adding style changes for drag and drop items

--- assets/js/script.js
+++ assets/js/script.js
@@ -191,6 +191,7 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
     
 };
@@ -213,7 +214,16 @@ var dropTaskHandler = function(event) {
         statusSelectEl.selectedIndex = 2;
     }
 
+    dropZoneEl.removeAttribute("style");
+
     dropZoneEl.appendChild(draggableElement);
+}
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
 }
 
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -225,3 +235,5 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
